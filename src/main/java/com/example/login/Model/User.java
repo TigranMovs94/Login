397bf667485664a0d59aed2ca525882196eff94d
TigranMovs94 <90642996+TigranMovs94@users.noreply.github.com>
2023-02-23 23:42:25 +0400

    New project

---
+++ src/main/java/com/example/login/Model/User.java
@@ -0,0 +1,71 @@
+package com.example.login.Model;
+
+public class User {
+
+    private String name;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private int age;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/example/login/RegisterController.java
+++ src/main/java/com/example/login/RegisterController.java
@@ -1,7 +1,11 @@
 package com.example.login;
 
+import com.example.login.Model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +13,14 @@ public class RegisterController {
 
     @RequestMapping("/registerPage")
     public String register(){
+
         return "registerPage";
+    }
+
+    @PostMapping ("/registerPage")
+    public String registerPeople(@ModelAttribute User user, Model model){
+        //System.out.println(user.toString());
+        model.addAttribute("name",user.getName());
+        return "Welcome";
     }
 }
