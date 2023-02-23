d2eba27b2b834006d7a986dcf7e4d90b1db6dbd7
TigranMovs94 <90642996+TigranMovs94@users.noreply.github.com>
2023-02-23 21:28:22 +0400

    New project

---
+++ src/main/java/com/example/login/IndexController.java
@@ -0,0 +1,24 @@
+package com.example.login;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+
+    }
+
+
+
+
+}
